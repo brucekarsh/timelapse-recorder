bb3ff53b7f78069804baf1f68206e4ede362b31b
Bruce Karsh <brucekarsh@gmail.com>
2019-10-02 22:59:55 -0700

    Add StartStop button.

--- timelapse_recorder.py
+++ timelapse_recorder.py
@@ -11,6 +11,7 @@ import threading
 import random
 import queue
 import signal
+from tkinter import ttk
 import sys
 
 class TimelapseRecorder:
@@ -47,38 +48,62 @@ class TimelapseRecorder:
     self.configFrame.pack(fill='x')
     self.configFrame.pack_forget()
 
-    startButton = tkinter.Button(self.buttonFrame, text="Start", command=self.start)
+    self.startStopButton = ttk.Button(
+            self.buttonFrame,
+            text="StartStop",
+            command=self.startStop)
+    self.startStopButton.pack(side=tkinter.LEFT, anchor=tkinter.W, fill='x')
+
+    im = Image.open('record-circle-fill.gif')
+    self.startPhoto = ImageTk.PhotoImage(im)
+    startButton = ttk.Button(
+            self.buttonFrame,
+            text="Start",
+            command=self.start,
+            image=self.startPhoto,
+            compound=tkinter.LEFT)
+    startButton.photo = self.startPhoto
     startButton.pack(side=tkinter.LEFT, anchor=tkinter.W, fill='x')
-    stopButton = tkinter.Button(self.buttonFrame, text="Stop", command=self.stop)
+
+    im = Image.open('stop-fill.gif')
+    self.stopPhoto = ImageTk.PhotoImage(im)
+    stopButton = ttk.Button(
+            self.buttonFrame,
+            text="Stop",
+            command=self.stop,
+            image=self.stopPhoto,
+            compound=tkinter.LEFT
+            )
+    stopButton.photo = self.stopPhoto
     stopButton.pack(side=tkinter.LEFT)
 
     self.showConfigStringVar = tkinter.StringVar()
-    self.showConfigButton = tkinter.Checkbutton(
+    self.showConfigButton = ttk.Checkbutton(
             self.buttonFrame, text='configure', variable=self.showConfigStringVar,
             command=self.showConfigButtonToggle, onvalue='on', offvalue='off')
     self.showConfigButton.pack(side=tkinter.RIGHT)
 
-    filePrefixLabel = tkinter.Label(self.configFrame, text='filename prefix')
+    filePrefixLabel = ttk.Label(self.configFrame, text='filename prefix')
     filePrefixLabel.pack(side=tkinter.LEFT)
     self.filePrefixStringVar = tkinter.StringVar()
     self.filePrefixStringVar.set(prefix)
     self.filePrefixStringVar.trace('w', self.filePrefixChange)
-    self.filePrefixEntry = tkinter.Entry(self.configFrame, textvariable=self.filePrefixStringVar, width=4)
+    self.filePrefixEntry = ttk.Entry(self.configFrame, textvariable=self.filePrefixStringVar, width=4)
     self.filePrefixEntry.pack(side=tkinter.LEFT)
 
-    outputDirectoryLabel = tkinter.Label(self.configFrame, text='output directory')
+    outputDirectoryLabel = ttk.Label(self.configFrame, text='output directory')
     outputDirectoryLabel.pack(side=tkinter.LEFT)
     self.outputDirectoryStringVar = tkinter.StringVar()
     self.outputDirectoryStringVar.set(outputDirectory)
     self.outputDirectoryStringVar.trace('w', self.outputDirectoryChange)
-    self.outputDirectoryEntry = tkinter.Entry(self.configFrame, textvariable=self.outputDirectoryStringVar)
+    self.outputDirectoryEntry = ttk.Entry(self.configFrame, textvariable=self.outputDirectoryStringVar)
     self.outputDirectoryEntry.pack(side=tkinter.LEFT)
 
-    self.imagelabel = tkinter.Label(self.root)
-    self.imagelabel.pack()
+    self.imageLabel = ttk.Label(self.root)
+    self.imageLabel.pack()
 
     frame_pil = ImageTk.PhotoImage(Image.fromarray(np.zeros( (480, 640) ) ))
-    self.imagelabel.configure(image=frame_pil)
+    self.imageLabel.configure(image=frame_pil)
 
     self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
     self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
@@ -89,6 +114,7 @@ class TimelapseRecorder:
     self.writebackConfig()
     print (self.makeFilename())
     self.out = None
+    self.makeStartStopButtonAStartButton()
     self.root.mainloop()
 
   def callback(self):
@@ -110,8 +136,8 @@ class TimelapseRecorder:
     self.out.write(frame)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_pil = ImageTk.PhotoImage(Image.fromarray(frame))
-    self.imagelabel.configure(image=frame_pil)
-    self.imagelabel.image = frame_pil
+    self.imageLabel.configure(image=frame_pil)
+    self.imageLabel.image = frame_pil
 
   def filePrefixChange(self, *args):
     text = self.filePrefixEntry.get()
@@ -137,18 +163,19 @@ class TimelapseRecorder:
       if self.showConfigStringVar.get() == 'on':
         print ("pack")
         self.buttonFrame.pack_forget()
-        self.imagelabel.pack_forget
+        self.imageLabel.pack_forget
 
         self.buttonFrame.pack(fill='x')
         self.configFrame.pack(fill='x')
-        self.imagelabel.pack(side=tkinter.BOTTOM)
+        self.imageLabel.pack(side=tkinter.BOTTOM)
       else:
         print ("pack_forget")
         self.configFrame.pack_forget()
 
   def shutdown(self):
     self.cap.release()
-    self.out.release
+    if self.out:
+      self.out.release()
     cv2.destroyAllWindows()
     print('exiting!')
     sys.exit(0)
@@ -157,17 +184,34 @@ class TimelapseRecorder:
     print('You pressed Ctrl+C!')
     self.shutdown()
 
+  def makeStartStopButtonAStartButton(self):
+      self.startStopButton.configure(
+          image=self.startPhoto, text='start', compound=tkinter.LEFT)
+
+  def makeStartStopButtonAStopButton(self):
+      self.startStopButton.configure(
+          image=self.stopPhoto, text='stop', compound=tkinter.LEFT)
+
   def start(self):
     filename = self.makeFilename()
     print ("start", filename)
+    self.makeStartStopButtonAStopButton()
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     self.out = cv2.VideoWriter(filename, fourcc, 30.0, (self.width, self.height))
     self.running = True
     self.root.after(self.callbackInterval, self.callback)
+
+  def startStop(self):
+    print ("startstop")
+    if self.running:
+      self.stop()
+    else:
+      self.start()
   
   def stop(self):
-      self.running = False
-      print ("stop")
+    self.makeStartStopButtonAStartButton()
+    self.running = False
+    print ("stop")
 
   def validateFilePrefixChange(self, text):
       # TODO WRITEM
